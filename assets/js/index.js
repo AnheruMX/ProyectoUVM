210dbe766a84f6572af30489f17420428673099a
Angel Cetz Varguez <80364343+AnheruMX@users.noreply.github.com>
2022-04-03 09:44:40 -0700

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,18 @@
-$(function(){
+'use strict'
 
-    $(document).on('click',function(e){
 
-        let elemento = $(e.target).attr('id');
-        console.log(elemento);
+document.querySelector('#miForm').addEventListener('submit', e => {
 
-        if(elemento == 'index-contenedor-modal-' || elemento == 'index-btn-cerrar-modal')
-        {
-            $('#index-contenedor-modal-').hide();
-        }      
-    });
+    e.preventDefault();
+    
+    let correo = document.querySelector("#Correo").value;
 
-    $('#Attach-AC-0121').on('click',function(e){
-        $('#index-contenedor-modal-').show();
-    });
-
-    $('#Attach-AC-0221').on('click',function(e){
-        $('#index-contenedor-modal-').show();
-    });
+    if(correo == '' || correo.indexOf("@") == -1)
+    {
+        document.querySelector("#loginError").setAttribute('style','display:flex'); 
+    }
+    else
+    {
+        window.location.href="main.html"
+    }
 });
